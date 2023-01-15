da1bc0b441b22a8c98d78c723ba36228a04e70d1
danivent1965 <dani.vent@orange.fr>
2023-01-15 11:47:33 +0100

    commit

--- variables.php
+++ variables.php
@@ -5,21 +5,25 @@ $users = [
         'full_name' => 'Dan Vent',
         'email' => 'dan.vent@laposte.net',
         'age' => 57,
+        'password' => 'password',
     ],
     [
         'full_name' => 'Mickaël Andrieu',
         'email' => 'mickael.andrieu@exemple.com',
         'age' => 34,
+        'password' => 'devine',
     ],
     [
         'full_name' => 'Mathieu Nebra',
         'email' => 'mathieu.nebra@exemple.com',
         'age' => 34,
+        'password' => 'MiamMiam',
     ],
     [
         'full_name' => 'Laurène Castor',
         'email' => 'laurene.castor@exemple.com',
         'age' => 28,
+        'password' => 'laCasto28',
     ],
 ];
 
